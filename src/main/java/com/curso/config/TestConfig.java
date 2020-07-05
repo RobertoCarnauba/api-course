b5e0ce0fa5be7c9df19079eea543f0a7899e04e1
RobertoCarnauba <betocarnauba@hotmail.com>
2020-07-05 14:34:21 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/curso/config/TestConfig.java
+++ src/main/java/com/curso/config/TestConfig.java
@@ -20,10 +20,11 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		User u1 = new User(1L, "Betim", "betim@bem", "12das3");
-		User u2 = new User(2L, "Betim", "betim@bem", "12das3");
+		User u1 = new User(1L, "Roberto Carnaúba Dias", "betom@bem","11961063734", "12das3");
+		User u2 = new User(2L, "Luiz Roberto Carnaúba", "luizroberto@bem","11961063734", "12das3");
+		User u3 = new User(3L, "Marcela Dias", "marceladias@bem","11961063734","12das3");
 
-		userRespositoy.saveAll(Arrays.asList(u1, u2));
+		userRespositoy.saveAll(Arrays.asList(u1, u2, u3));
 		
 	}
 
