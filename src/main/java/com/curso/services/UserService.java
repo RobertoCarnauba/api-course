f1b4e3c71b8090a4d972bef74e53ac418b328678
RobertoCarnauba <betocarnauba@hotmail.com>
2020-07-09 23:31:02 -0300

    Tratamento error [Delete]

--- src/main/java/com/curso/services/UserService.java
+++ src/main/java/com/curso/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.curso.entities.User;
 import com.curso.repositories.UserRespositoy;
+import com.curso.services.exceptions.DatabaseException;
 import com.curso.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,9 +31,15 @@ public class UserService {
 	public User insert(User obj) {
 		return userRespositoy.save(obj);
 	}
-	
+	//Tratamento de error
 	public void delete(Long id) {
-		userRespositoy.deleteById(id);
+		try {
+			userRespositoy.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
